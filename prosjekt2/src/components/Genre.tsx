1b45c04da21d868a73312aac402e542cc1f1a329
Bendik Norli <bendik.norli@ntnu.no>
2023-10-11 08:56:55 +0200

    feat: enable selecting genre

--- prosjekt2/src/components/Genre.tsx
+++ prosjekt2/src/components/Genre.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import '../css/Genre.css';
 
-export default function Genre(props: { genre: string }) {
-  return <button className="genre-button">{props.genre}</button>;
+export default function Genre(props: {
+  genre: string;
+  isSelected: boolean;
+  selectGenre: (genre: string) => void;
+}) {
+  const selectGenre = (genre: string) => {
+    props.selectGenre(genre);
+  };
+
+  return (
+    <button
+      className={props.isSelected ? 'selected-genre-button' : 'genre-button'}
+      onClick={selectGenre.bind(null, props.genre)}
+    >
+      {props.genre}
+    </button>
+  );
 }
