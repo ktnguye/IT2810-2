e2a2f46e1f9455354e5cb54575a45a5749a4f360
Bendik Norli <bendik.norli@ntnu.no>
2023-10-11 09:04:13 +0200

    fix: export interfaces

--- prosjekt2/src/components/SongFeed.tsx
+++ prosjekt2/src/components/SongFeed.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import Song from '../components/Song';
 import '../css/SongFeed.css';
+import { SongInterface } from '../types/interfaces';
 
-export default function SongFeed(props: { songs: Song[] }) {
+export default function SongFeed(props: { songs: SongInterface[] }) {
   return (
     <div className="song-feed">
       {props.songs.map((song) => (
