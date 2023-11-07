c4ceb4493a779ea48ba638f2b5a78c220ea6fb12
Bendik Norli <bendik.norli@ntnu.no>
2023-11-07 18:14:23 +0100

    feat: display lyrics tags and views in song display

--- prosjekt2/frontend/src/components/SongDisplay.tsx
+++ prosjekt2/frontend/src/components/SongDisplay.tsx
@@ -3,16 +3,26 @@ import { SongInterface } from '../types/interfaces';
 import { Link } from 'react-router-dom';
 
 export default function SongDisplay(props: { song: SongInterface }) {
+  const lyrics = props.song.lyrics.split('\n').map((line, index) => {
+    return <p key={index}>{line}</p>;
+  });
+
   return (
     <div className="song-display">
       <Link to={'/project2/'} className="back-button">
         {'<-'}
       </Link>
-      <h2 className="song-display-title">{props.song.title}</h2>
       <div className="song-display-info">
+        <h2 className="song-display-title">{props.song.title}</h2>
         <h3 className="song-display-artist">
           {props.song.artist} ({props.song.year})
         </h3>
+        <p>Tag: {props.song.tag.toUpperCase()}</p>
+        <p>Views: {props.song.views}</p>
+      </div>
+      <div className="song-display-lyrics">
+        <h3>Lyrics</h3>
+        <div>{lyrics}</div>
       </div>
     </div>
   );
