9dfe647ada9d38a36fdc11135f64a44cbaa4c7cb
Bendik Norli <bendik.norli@ntnu.no>
2023-11-20 16:59:44 +0100

    style: fix text wrapping for reviews

--- prosjekt2/frontend/src/components/Review/Review.tsx
+++ prosjekt2/frontend/src/components/Review/Review.tsx
@@ -4,10 +4,9 @@ import '../../css/Review.css';
 export default function Review(props: { review: ReviewInterface }) {
   return (
     <div className="review-box">
+      <h2>{props.review.name}</h2>
       <div className="review-header">
-        <h2>
-          {props.review.name} ({props.review.rating}/5)
-        </h2>
+        <p>({props.review.rating}/5)</p>
         <p>{props.review.date.toString().split('T')[0]}</p>
       </div>
       <p>{props.review.review}</p>
