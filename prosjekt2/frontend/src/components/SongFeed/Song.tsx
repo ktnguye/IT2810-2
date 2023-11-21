c691e477fee005860ba913f04048fa810bdd429b
Bendik Norli <bendik.norli@ntnu.no>
2023-11-21 13:52:23 +0100

    fix: ensure header-levels decreases by one

--- prosjekt2/frontend/src/components/SongFeed/Song.tsx
+++ prosjekt2/frontend/src/components/SongFeed/Song.tsx
@@ -12,8 +12,8 @@ export default function Song(props: { song: SongInterface; color: string }) {
       <div className="song-cover-fade"></div>
       <div className="song-info">
         <h2 className="song-title">{props.song.title}</h2>
-        <h4 className="song-artist">{props.song.artist}</h4>
-        <h5 className="song-tags">{props.song.tag}</h5>
+        <h3 className="song-artist">{props.song.artist}</h3>
+        <p className="song-tags">{props.song.tag}</p>
       </div>
     </Link>
   );
