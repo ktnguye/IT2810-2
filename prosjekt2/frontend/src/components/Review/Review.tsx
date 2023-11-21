ffdb3e90febca6655f2d4dcc82c83f93ed2b17de
Bendik Norli <bendik.norli@ntnu.no>
2023-11-21 12:48:57 +0100

    feat: enable deleting reviews

--- prosjekt2/frontend/src/components/Review/Review.tsx
+++ prosjekt2/frontend/src/components/Review/Review.tsx
@@ -1,9 +1,26 @@
 import { ReviewInterface } from '../../types/interfaces';
 import './Review.css';
+import { useMutation } from '@apollo/client';
+import { DELETE_REVIEW } from '../../graphql/mutations';
 
 export default function Review(props: { review: ReviewInterface }) {
+  const [deleteReview] = useMutation(DELETE_REVIEW, {
+    variables: { id: props.review._id },
+  });
+
+  const handleDelete = () => {
+    console.log('Deleting review with _id: ' + props.review._id);
+    console.log(props.review);
+    deleteReview();
+
+    window.location.reload();
+  };
+
   return (
     <div className="review-box">
+      <button className="delete-button" onClick={handleDelete}>
+        X
+      </button>
       <h2>{props.review.name}</h2>
       <div className="review-header">
         <p>({props.review.rating}/5)</p>
