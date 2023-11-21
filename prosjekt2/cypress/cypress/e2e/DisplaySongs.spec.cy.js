d6a8e394694da37e41d38e232446a0f81f1ee5d7
Ken Nguyen <ktnguye@stud.ntnu.no>
2023-11-21 13:02:24 +0100

    test: e2e tests for displaying songs

--- prosjekt2/cypress/cypress/e2e/DisplaySongs.spec.cy.js
+++ prosjekt2/cypress/cypress/e2e/DisplaySongs.spec.cy.js
@@ -1,22 +1,29 @@
 describe('navigate page', () => {
-  it('passes for visiting three different songs with two different ways to go to home', () => {
+  it('passes for visiting three different songs', () => {
     cy.visit('http://localhost:5173/project2');
     cy.get('.song-card').first().click();
     cy.get('.song-display-title').should('contain', 'Despacito Remix');
     cy.get('.back-button').click();
     cy.get('.song-card').eq(1).click();
     cy.get('.song-display-title').should('contain', 'Rap God');
-    cy.get('.side-bar-logo').click();
-    cy.get('.song-card').eq(6).click();
-    cy.get('.song-display-title').should('contain', 'Bohemian Rhapsody');
+    cy.get('.back-button').click();
+    cy.get('.load-more-button').click();
+    cy.get('.song-card').should('contain', 'rockstar');
+    cy.get('.song-card').should('have.length', 24);
   });
 
   it('passes for filtering and sorting', () => {
     cy.visit('http://localhost:5173/project2');
-    cy.get('.tag-button').eq(3).click();
+    cy.get('.tag-button').eq(4).click();
     cy.get('.order-button').click();
     cy.contains('Views: Least first').click();
-    cy.get('.song-card').eq(4).should('contain', 'Paranoia').click();
-    cy.get('.song-display-title').should('contain', 'Paranoia');
+    cy.get('.song-card').first().should('contain', 'Hold Me');
+    cy.get('.selected-tag-button').click();
+    cy.get('.song-card').first().should('not.contain', 'Hold Me');
+
+    cy.get('.order-button').click();
+    cy.contains('Name: Z-A').click();
+    cy.get('.song-card').first().should('contain', '﹤3').click();
+    cy.get('.song-display-title').should('contain', '﹤3');
   });
 });
